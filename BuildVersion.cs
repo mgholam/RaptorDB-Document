e7352cce739137d529c503561c7cd0eded64afad
M. Gholam <mgholam@gmail.com>
2018-01-26 17:16:35 +0330

    v3.3.19.1 - bug fix shutting down in .net core on ubuntu

--- BuildVersion.cs
+++ BuildVersion.cs
@@ -1,6 +1,6 @@
 using System.Reflection;
-// build number = 440
-// build version = 3.3.18
+// build number = 442
+// build version = 3.3.19
 
 [assembly: AssemblyVersion("3.0.0.0")]
-[assembly: AssemblyFileVersion("3.3.18.440")]
+[assembly: AssemblyFileVersion("3.3.19.442")]
